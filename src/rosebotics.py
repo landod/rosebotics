bdef7a1869af5f8771d7e88e0e9b181bb5b8fb41
leeem <hs162a2@gmail.com>
2018-10-03 16:00:35 -0400

    идти к гулаг блять
    fixed style error

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -29,6 +29,7 @@ class Snatch3rRobot(object):
         self.left_wheel.stop_spinning(stop_action)
         self.right_wheel.stop_spinning(stop_action)
 
+
 class Wheel(object):
     def __init__(self, port, default_duty_cycle_percent=100,
                  default_stop_action=StopAction.BRAKE.value):
